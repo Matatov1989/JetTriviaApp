e759327795ab3ce09492b965001b365f5e728f43
Yury <matatov1989@gmail.com>
2023-04-13 11:55:52 +0300

    finishing up the repository class

--- app/src/main/java/com/example/jettriviaapp/repository/QuestionRepository.kt
+++ app/src/main/java/com/example/jettriviaapp/repository/QuestionRepository.kt
@@ -1,13 +1,28 @@
 package com.example.jettriviaapp.repository
 
 import android.media.session.MediaSession.QueueItem
+import android.util.Log
 import com.example.jettriviaapp.data.DataOrException
 import com.example.jettriviaapp.network.QuestionAPI
 import javax.inject.Inject
 
 class QuestionRepository @Inject constructor(private val api: QuestionAPI) {
 
-    private val listOfQuestions = DataOrException<ArrayList<QueueItem>, Boolean, Exception>()
+    private val dataOrException = DataOrException<ArrayList<QueueItem>, Boolean, Exception>()
+
+    suspend fun getAllQuestions(): DataOrException<ArrayList<QueueItem>, Boolean, Exception> {
+        try {
+            dataOrException.loading = true
+            dataOrException.data = api.getAllQuestions()
+            if (dataOrException.data.toString().isNotEmpty())
+                dataOrException.loading = false
+
+        } catch (exception: Exception) {
+            dataOrException.e = exception
+            Log.d("Exception", "getAllQuestions :")
+        }
+        return dataOrException
+    }
 
 
 }
