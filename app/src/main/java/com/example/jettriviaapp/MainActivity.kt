facf34fd4fe90a2abf1444b66788fe94d512d360
Yury <matatov1989@gmail.com>
2023-04-13 13:21:49 +0300

    testing & fixing viewmodel

--- app/src/main/java/com/example/jettriviaapp/MainActivity.kt
+++ app/src/main/java/com/example/jettriviaapp/MainActivity.kt
@@ -1,17 +1,21 @@
 package com.example.jettriviaapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.jettriviaapp.screens.QuestionsViewModel
 import com.example.jettriviaapp.ui.theme.JetTriviaAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,7 +26,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    TriviaHome()
+
                 }
             }
         }
@@ -30,14 +35,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TriviaHome(viewModel: QuestionsViewModel = hiltViewModel()) {
+    Questions(viewModel)
+
 }
+
+@Composable
+fun Questions(viewModel: QuestionsViewModel) {
+
+    val questions = viewModel.data.value.data?.toMutableList()
+
+    if (viewModel.data.value.loading == true) {
+
+    } else {
+
+    }
+
+    Log.d("Questions","Questions: ${questions?.size}")
+
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetTriviaAppTheme {
-        Greeting("Android")
+
     }
 }
