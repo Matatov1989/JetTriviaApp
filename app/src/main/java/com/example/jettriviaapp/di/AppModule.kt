592278b5b388b230ee76be47b3ade74d9b6caa78
Yury <matatov1989@gmail.com>
2023-04-13 11:04:58 +0300

    adding hilt classes & retrofit

---
+++ app/src/main/java/com/example/jettriviaapp/di/AppModule.kt
@@ -0,0 +1,28 @@
+package com.example.jettriviaapp.di
+
+import androidx.compose.ui.unit.Constraints
+import com.example.jettriviaapp.network.QuestionAPI
+import com.example.jettriviaapp.util.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideQuestionApi(): QuestionAPI {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(QuestionAPI::class.java)
+    }
+
+}
