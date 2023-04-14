f64b70f570b4297de657742fdc2e0b81d2dee7a2
Yury <matatov1989@gmail.com>
2023-04-14 17:59:13 +0300

    finalizing

--- app/src/main/java/com/example/jettriviaapp/component/Questions.kt
+++ app/src/main/java/com/example/jettriviaapp/component/Questions.kt
@@ -7,10 +7,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.ButtonDefaults.buttonColors
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -22,6 +19,7 @@ import androidx.compose.ui.text.ParagraphStyle
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextIndent
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -101,7 +99,9 @@ fun QuestionDisplay(
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.Start
         ) {
-            QuestionTracker(counter = questionIndex.value)
+            if (questionIndex.value >= 3) ShowProgress(score = questionIndex.value)
+
+            QuestionTracker(counter = questionIndex.value, viewModel.getTotalQuestionCount())
             DrawDottedLine(pathEffect = pathEffect)
 
             Column {
@@ -248,6 +248,10 @@ fun ShowProgress(score: Int = 12) {
 
     val gradient = Brush.linearGradient(listOf(Color(0xFFF95075), Color(0xFFBE6BE5)))
 
+    val progressFactor by remember(score) {
+        mutableStateOf(score * 0.005f)
+    }
+
     Row(
         modifier = Modifier
             .padding(3.dp)
@@ -276,7 +280,9 @@ fun ShowProgress(score: Int = 12) {
         Button(
             contentPadding = PaddingValues(1.dp),
             onClick = {},
-            modifier = Modifier.fillMaxWidth().background(brush = gradient),
+            modifier = Modifier
+                .fillMaxWidth(progressFactor)
+                .background(brush = gradient),
             elevation = null,
             enabled = false,
             colors = buttonColors(
@@ -285,6 +291,16 @@ fun ShowProgress(score: Int = 12) {
             )
 
         ) {
+            Text(
+                text = (score * 10).toString(),
+                modifier = Modifier
+                    .clip(shape = RoundedCornerShape(23.dp))
+                    .fillMaxHeight(0.87f)
+                    .fillMaxWidth()
+                    .padding(6.dp),
+                color = AppColors.mOffWhite,
+                textAlign = TextAlign.Center
+            )
 
         }
 
